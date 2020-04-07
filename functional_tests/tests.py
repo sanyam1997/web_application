e41b40930e69206b6fbb27395f64be4466a79edc
srajpal <srajpal@iu.edu>
2020-04-07 13:52:21 -0400

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,7 +7,7 @@ MAX_WAIT = 10
 import time
 # import unittest
 
-class NewVisitorTest( LiveServerTestCase) :
+class NewVisitorTest( LiveServerTestCase ) :
 
     def setUp( self ) :
         self.browser = webdriver.Firefox( )
@@ -19,7 +19,6 @@ class NewVisitorTest( LiveServerTestCase) :
         table = self.browser.find_element_by_id( "id_list_table" )
         rows = table.find_elements_by_tag_name( "tr" )
         self.assertIn( row_text , [ row.text for row in rows ] )
-
 
     def test_can_start_a_list_for_one_user( self ):
     # def test_can_start_a_list_and_retrieve_it_later( self ) :
@@ -73,14 +72,13 @@ class NewVisitorTest( LiveServerTestCase) :
         # He visits the URL - his to-do list is still there.
         # The page updates again, and now shows both items on his list
 
-
-
     def test_multiple_users_can_start_lists_at_different_urls( self ) :
         # Sanyam has a new to-do list
         self.browser.get( self.live_server_url )
         inputbox = self.browser.find_element_by_id( "id_new_item" )
         inputbox.send_keys( "Buy peacock feathers" )
         inputbox.send_keys( Keys.ENTER )
+        self.wait_for_row_in_list_table( "1: Buy peacock feathers" )
 
         # He notices that his list has a unique URL
         sanyam_list_url = self.browser.current_url
@@ -110,7 +108,7 @@ class NewVisitorTest( LiveServerTestCase) :
         # Francis got his own URL
         francis_list_url = self.browser.current_url
         self.assertRegex( francis_list_url , '/lists/.+' )
-        self.assertNotEqual( francis_list_url )
+        self.assertNotEqual( francis_list_url , sanyam_list_url )
 
         # Again, there is no trace of Sanyam's URL
         page_text = self.browser.find_element_by_tag_name( "body" ).text
