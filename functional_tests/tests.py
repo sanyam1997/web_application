378d197e1243da0e7b1f68914f71f97f8b6ab3b9
srajpal <srajpal@iu.edu>
2020-04-08 13:05:49 -0400

    Use Bootstrap to improve layout and refactor templates to use base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -7,7 +7,7 @@ MAX_WAIT = 10
 import time
 # import unittest
 
-class NewVisitorTest( LiveServerTestCase ) :
+class NewVisitorTest( StaticLiveServerTestCase ) :
 
     def setUp( self ) :
         self.browser = webdriver.Firefox( )
@@ -62,8 +62,6 @@ class NewVisitorTest( LiveServerTestCase ) :
         # The page updates again, and now shows both items on his list
         self.check_for_row_in_list_table( "1: Buy peacock feathers" )
         self.check_for_row_in_list_table( "2: Use peacock feathers to make a fly" )
-
-        self.fail( "Finish the test." )
 
         # Sanyam wonders whether the site still remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
@@ -135,16 +133,21 @@ class NewVisitorTest( LiveServerTestCase ) :
         self.browser.set_window_size( 1024 , 768 )
 
         # He notices that the input box is nicely centered
+        # inputbox = self.browser.find_element_by_id( "id_new_item" )
         inputbox = self.browser.find_element_by_id( "id_new_item" )
+        self.assertAlmostEqual(
+            inputbox.location[ 'x' ] + inputbox.size['width'] / 2 ,
+            512 ,
+            delta = 10
+        )
         # He starts a new list and sees the input
         # is centered too
         inputbox.send_keys( "testing" )
         inputbox.send_keys( Keys.ENTER )
         self.wait_for_row_in_list_table( "1: testing" )
         inputbox = self.browser.find_element_by_id( "id_new_item" )
-
         self.assertAlmostEqual(
-            inputbox.location[ 'x' ] + inputbox.size[ 'width' ] / 2 ,
+            inputbox.location[ 'x' ] + inputbox.size['width'] / 2 ,
             512 ,
             delta = 10
         )
