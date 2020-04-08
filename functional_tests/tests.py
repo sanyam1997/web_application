6f8ce9774c7bf75a96590475c3a40b0a8ca3baf6
srajpal <srajpal@iu.edu>
2020-04-08 11:56:46 -0400

    First steps of FT for laying + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -129,6 +129,22 @@ class NewVisitorTest( LiveServerTestCase ) :
                     raise e
                 time.sleep(0.5)
 
+    def test_layout_and_styling( self ) :
+        # Sanyam goes to the home page
+        self.browser.get( self.live_server_url )
+        self.browser.set_window_size( 1024 , 768 )
 
-# if __name__ == "__main__" :
-#     unittest.main( warnings = 'ignore' )
+        # He notices that the input box is nicely centered
+        inputbox = self.browser.find_element_by_id( "id_new_item" )
+        # He starts a new list and sees the input
+        # is centered too
+        inputbox.send_keys( "testing" )
+        inputbox.send_keys( Keys.ENTER )
+        self.wait_for_row_in_list_table( "1: testing" )
+        inputbox = self.browser.find_element_by_id( "id_new_item" )
+
+        self.assertAlmostEqual(
+            inputbox.location[ 'x' ] + inputbox.size[ 'width' ] / 2 ,
+            512 ,
+            delta = 10
+        )
