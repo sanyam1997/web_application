663f1bdcca5e499064324b918403fee393327635
srajpal <srajpal@iu.edu>
2020-04-18 17:55:34 -0400

    Adjust new list view to do model validation and Refactor list view to handle new item POSTs

--- lists/views.py
+++ lists/views.py
@@ -1,28 +1,27 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from lists.models import Item , List
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 def home_page( request ) :
-
-    # if request.method == 'POST' :
-    #     Item.objects.create( text = request.POST[ "item_text" ] )
-    #     return redirect( "/lists/the-only-list-in-the-world/" )
-
     return render( request , "home.html" )
 
 def view_list( request , list_id ) :
     list_ = List.objects.get( id = list_id )
-    # items = Item.objects.filter( list = list_ )
+    if request.method == 'POST' :
+        Item.objects.create( text = request.POST[ 'item_text' ] , list = list_ )
+        return redirect( f'/lists/{list_.id}/' )
     return render( request , "list.html" , { "list" : list_ } )
 
 def new_list( request ) :
     list_ = List.objects.create( )
-    Item.objects.create( text = request.POST[ "item_text" ] , list = list_ )
+    item = Item( text = request.POST[ 'item_text' ] , list = list_ )
+    try :
+        item.full_clean( )
+        item.save( )
+    except ValidationError :
+        list_.delete( )
+        error = "You can't have an empty list item"
+        return render( request , 'home.html' , { "error" : error } )
     return redirect( f'/lists/{list_.id}/' )
-
-def add_item( request , list_id ) :
-    list_ = List.objects.get( id = list_id )
-    Item.objects.create( text = request.POST[ "item_text" ] , list = list_ )
-    return redirect(f'/lists/{list_.id}/')
-    pass
